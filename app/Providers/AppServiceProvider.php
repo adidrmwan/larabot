9ac7ff58a9d64ca466416591128cfaaecc12d952
Adi Darmawan <adidarmawan@ADMINs-MacBook-Pro.local>
2024-12-20 11:16:48 +0700

    force schemes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,16 +12,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        
+
     }
 
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot(UrlGenerator $url): void
     {
-        //
-    }
-
-    
+        if(env('APP_ENV') !== 'local')
+        {
+            $url->forceScheme('https');
+        }
+    }  
 }
